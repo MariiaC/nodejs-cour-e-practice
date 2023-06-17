667b2ab29db227bb05225ddda7d0eefff37f37af
MariiaC <mariiachvorun@gmail.com>
2023-06-17 21:56:41 +0300

    code example

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello world')
+// console.log('Hello world')
+const express = require('express');
+const mongoose = require('mongoose');
+const authRouter = require('./authRouter.js');
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+const start = async() => {
+    try {
+        await mongoose.connect(`mongodb+srv://user:user@cluster0.ref48dv.mongodb.net/?retryWrites=true&w=majority`);
+        app.listen(PORT, () => {
+    console.log(`server started on PORT ${PORT}`)
+})
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+start()
