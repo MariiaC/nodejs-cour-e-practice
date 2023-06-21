110ac671aa543fd9e0ee26961277ed6347499f59
MariiaC <mariiachvorun@gmail.com>
2023-06-21 13:08:55 +0300

    upd-contacts

--- contacts.js
+++ contacts.js
@@ -1,27 +1,32 @@
-const fs = require('fs/promises');
+const fs = require("fs/promises");
+const path = require("path");
 
 
-
-const contactsPath = 
-
-
-  function listContacts() { }
-
-function getContactById(contactId) { }
-
-function removeContact(contactId) {
-
-}
+const contactsPath = path.join(__dirname, "contacts.json");
 
 
+//функції для роботи з контактами
 
-function addContact(name, email, phone) {}
+const listContacts = async () => {
+  const allContacts = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(allContacts);
+ };
 
+const getContactById = (id) => { 
+  const contacts = listContacts();
+  const contactById = contacts.find(contact => contact.id === id);
+  return contactById || null;
+};
 
+const removeContact = (id) => {
 
+};
 
+const addContact=(name, email, phone)=> {};
 
-
-  module.exports = {
-  
-}
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
