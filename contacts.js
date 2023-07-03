bdf37fdf17f109bad803ec10887c2cece4bc2e82
MariiaC <mariiachvorun@gmail.com>
2023-07-03 22:07:29 +0300

    npm-and-contacts-upd

--- contacts.js
+++ contacts.js
@@ -1,28 +1,55 @@
-// index.js
-const argv = require('yargs').argv;
+const express = require('express')
 
-// TODO: рефакторити
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      // ...
-      break;
+const router = express.Router()
 
-    case 'get':
-      // ... id
-      break;
+router.get('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-    case 'add':
-      // ... name email phone
-      break;
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-    case 'remove':
-      // ... id
-      break;
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
-  }
-}
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
 
-invokeAction(argv);
+router.put('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+module.exports = router
+
+
+// // index.js
+// const argv = require('yargs').argv;
+
+// // TODO: рефакторити
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case 'list':
+//       // ...
+//       break;
+
+//     case 'get':
+//       // ... id
+//       break;
+
+//     case 'add':
+//       // ... name email phone
+//       break;
+
+//     case 'remove':
+//       // ... id
+//       break;
+
+//     default:
+//       console.warn('\x1B[31m Unknown action type!');
+//   }
+// }
+
+// invokeAction(argv);
